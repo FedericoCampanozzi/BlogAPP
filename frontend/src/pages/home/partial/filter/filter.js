eaeb7179d07963f7ce21e66966e950e40e55b842
Federico Campanozzi <federico.campanozzi@gmail.com>
2024-01-01 14:56:22 +0100

    Do some stuff with css

--- frontend/src/pages/home/partial/filter/filter.js
+++ frontend/src/pages/home/partial/filter/filter.js
@@ -2,40 +2,53 @@ import React from "react";
 import Form from "react-bootstrap/Form";
 import { useSharedState } from "../../../../shared/state-context";
 
+const insert = (arr, index, ...newItems) => [
+  // part of the array before the specified index
+  ...arr.slice(0, index),
+  // inserted items
+  ...newItems,
+  // part of the array after the specified index
+  ...arr.slice(index),
+];
+
 const PostsFilter = () => {
-    const { posts, setPosts, topics } = useSharedState();
-    const t = [...topics];
-    return (
-        <div key={"filter_00"}>
-            <Form.Label>Topics</Form.Label>
-            <Form.Select>
-                {
-                    t.map((t_el, index)=>{
-                        return (
-                            <option value={t_el}>{t_el}</option>
-                        )
-                    })
-                }
-            </Form.Select>
-            <Form.Label htmlFor="dateFrom">From</Form.Label>
-            <Form.Control
-                type="date"
-                id="dateFrom"
-            />
-            <Form.Label htmlFor="dateTo">To</Form.Label>
-            <Form.Control
-                type="date"
-                id="dateTo"
-            />
-            <Form.Label>Sort</Form.Label>
-            <Form.Select>
-                <option value="1">Most Liked</option>
-                <option value="1">Category</option>
-                <option value="2">Newer</option>
-                <option value="3">Oldest</option>
-            </Form.Select>
-        </div>
-    );
+  const { topics } = useSharedState();
+  let t = [...topics];
+  t = insert(t, 0, "ALL");
+
+  const sortMethodChanged = () => {};
+  const filteredDataChanged = () => {};
+  return (
+    <div key={"filter_00"}>
+      <Form key={"form_00"}>
+        <Form.Label>Topics</Form.Label>
+        <Form.Select>
+          {t.map((t_el, index) => {
+            return (
+              <option key={index} value={t_el}>
+                {t_el}
+              </option>
+            );
+          })}
+        </Form.Select>
+        <Form.Label htmlFor="dateFrom">From</Form.Label>
+        <Form.Control
+          type="date"
+          id="dateFrom"
+          onChange={filteredDataChanged}
+        />
+        <Form.Label htmlFor="dateTo">To</Form.Label>
+        <Form.Control type="date" id="dateTo" onChange={filteredDataChanged} />
+        <Form.Label>Sort</Form.Label>
+        <Form.Select onChange={sortMethodChanged}>
+          <option value="1">Most Liked</option>
+          <option value="2">Category</option>
+          <option value="3">Newer</option>
+          <option value="4">Oldest</option>
+        </Form.Select>
+      </Form>
+    </div>
+  );
 };
 
 export default PostsFilter;
--- frontend/src/pages/home/partial/post/post.js
+++ frontend/src/pages/home/partial/post/post.js
@@ -2,58 +2,93 @@ import React from "react";
 import { Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronLeft, faHeart } from "@fortawesome/free-solid-svg-icons";
-import Card from 'react-bootstrap/Card';
-import './post.css';
+import {
+  faChevronLeft,
+  faHeart,
+  faTrash,
+} from "@fortawesome/free-solid-svg-icons";
+import Card from "react-bootstrap/Card";
+import { useSharedState } from "../../../../shared/state-context";
+import "./post.css";
 
 const PostCard = ({ post, isDetail }) => {
   const navigate = useNavigate();
+  const { posts } = useSharedState();
   const goToDetail = () => {
-    navigate('/post-detail',  {
+    navigate("/post-detail", {
       state: {
-        post
-      }
+        post,
+      },
     });
   };
-
+  const getFormattedDate = (d) => {
+    return new Date(d).toISOString().split("T")[0];
+  };
   return (
-    <div>
+    <div key={post.id}>
+      {isDetail ? (
+        <>
+          <Button variant="outline-dark" onClick={() => navigate("/")}>
+            <FontAwesomeIcon icon={faChevronLeft} />
+          </Button>
+          <Button variant="outline-danger" onClick={() => navigate("/")}>
+            <FontAwesomeIcon icon={faTrash} />
+          </Button>
+          <Button variant="outline-dark" onClick={() => navigate("/")}>
+            <FontAwesomeIcon icon={faHeart} />
+          </Button>
+        </>
+      ) : null}
+      <Card className="card-container">
+        <Card.Body>
+          <Card.Title>{post.title}</Card.Title>
+          <Card.Title className="card-topic">
+            <div
+              className="image-profile"
+              style={{backgroundImage: `url("${post.publisher.imageProfileURL}")`}}
+            >
+            </div>
+            <div>
+              <span>{post.publisher.name}</span>
+              &nbsp;
+              <span>{post.publisher.surname}</span>
+            </div>
+          </Card.Title>
+          <Card.Subtitle className="mb-2 text-muted my-subtitle">
+            <span className="post-data">
+              {getFormattedDate(post.dateCreation.$date)}
+            </span>
+            <span className="post-topic">@{post.topic}</span>
+          </Card.Subtitle>
+          <Card.Text>
+            {isDetail ? <span>{post.summary}</span> : <span>{post.text}</span>}
+          </Card.Text>
+          {!isDetail ? (
+            <Button variant="outline-dark" onClick={goToDetail}>
+              Detail
+            </Button>
+          ) : null}
+          <div className="post-likes">
+            <FontAwesomeIcon icon={faHeart} /> <span>{post.likes}</span>
+          </div>
+        </Card.Body>
+      </Card>
       {
         isDetail ? (
-        <Button variant="info" onClick={() => navigate("/")}>
-          <FontAwesomeIcon icon={faChevronLeft} />
-        </Button>
+          <>
+            <h1>Related Post</h1>
+            {
+              posts?.map((post, index) => {
+                return (
+                  <div key={`p_${index}`}>
+                    <PostCard post={post} isDetail={false} />
+                  </div>
+                );
+              })
+            }
+          </>
         ) : null
       }
-      <Card className="card-container">
-      <Card.Body>
-        <Card.Title>{ post.Title }</Card.Title>
-        <Card.Title className="card-topic">
-          <div className="image-profile"> </div>
-          John Smit
-        </Card.Title>
-        <Card.Subtitle className="mb-2 text-muted my-subtitle">
-          <span>
-            22-10-2023
-          </span>
-          <span>
-            Math
-          </span>
-        </Card.Subtitle>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        {
-          !isDetail ?(
-            <Button variant="outline-dark" onClick={goToDetail}> Detail </Button>
-          ):null
-        }
-        <div className="post-likes">
-          <FontAwesomeIcon icon={faHeart} /> 1500
-        </div>
-      </Card.Body>
-    </Card>
     </div>
   );
 };
