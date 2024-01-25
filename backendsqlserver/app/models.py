bdbca6050661cea4929f6820ebdf33db3d196758
Federico Campanozzi <federico.campanozzi@gmail.com>
2024-01-25 23:23:57 +0100

    Added new Model

---
+++ backendsqlserver/app/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+class Topic(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    dateOfBirthday = models.DateField()
+    username = models.CharField(max_length=50)
+    imageProfileURL = models.CharField(max_length=1000, blank=True, null=True)
+    email = models.EmailField(max_length=254)
+    password = models.CharField(max_length=10)
+
+class Post(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=50)
+    dateCreation = models.DateField(auto_now_add=True)
+    summary = models.CharField(max_length=500)
+    text = models.CharField(max_length=2000, blank=True, null=True)
+    publisher = models.ForeignKey(User, on_delete=models.CASCADE, related_name='fk_user')
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name='fk_topic')
+    likes = models.IntegerField()
